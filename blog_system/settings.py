16b1d7c5f816bac93753ee909eebbc2120ac0b19
xjn <xjn@xjn.local>
2021-01-13 08:36:11 +0800

    V1.0.4 统一mysql密码

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db_blog',  # 要连接的数据库，连接前需要创建好
         'USER': 'root',  # 连接数据库的用户名
-        'PASSWORD': '123456',  # 连接数据库的密码
-        'HOST': '127.0.0.1',  # 连接主机，默认本级
+        'PASSWORD': '12345678',  # 连接数据库的密码
+        'HOST': '0.0.0.0',  # 连接主机，默认本级
         'PORT': 3306  # 端口 默认3306
     }
 }
